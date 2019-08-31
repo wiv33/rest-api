898a05931fd0bf38b6f1940dfcd3d2f126993135
PSK <wiv.psk@gmail.com>
2019-09-01 00:41:09 +0900

    add comment

--- src/main/java/com/psawesome/demorestapi/events/EventValidator.java
+++ src/main/java/com/psawesome/demorestapi/events/EventValidator.java
@@ -12,14 +12,15 @@ public class EventValidator {
     public void validate(EventDto eventDto, Errors errors) {
         if (eventDto.getBasePrice() > eventDto.getMaxPrice()
                 && eventDto.getMaxPrice() != 0) {
-            errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong.");
-            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is wrong.");
+            //global error에 담는다
+            errors.reject("wrongPrices", "Values for prices are wrong");
         }
 
         @NotNull LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
         if (endEventDateTime.isBefore(eventDto.getBeginEventDateTime())
                 || endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime())
                 || endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())) {
+            //field error에 담는다.
             errors.rejectValue("endEventDateTime", "wrongValue", "endEventDateTime wrong value");
         }
 
